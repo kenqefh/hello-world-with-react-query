03880e4443c68fc6e56de53e16c0f64c0f9bc314
Frank Dominguez <elvisf.dominguez@gmail.com>
2022-01-16 14:26:54 -0500

    Render the superheroes from the mock back-end in SuperHerores view

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,9 +1,34 @@
-// external imports
-// local imports
-// components imports
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SuperHeroesPage = (props) => {
-  return <div>SuperHeroesPage</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>Super Heroes Page</h2>
+      {data.map((heroe) => {
+        return (
+          <div key={heroe.id}>
+            <h3>{heroe.name}</h3>
+            <p>{heroe.alterEgo}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SuperHeroesPage;
