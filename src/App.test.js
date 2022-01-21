7ebfb4cc4c38e0921f6c878520d64828028c2f40
Frank Dominguez <elvisf.dominguez@gmail.com>
2022-01-21 08:25:21 -0500

    Update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('home', () => {
+  test('should render the app logo', () => {
+    render(<App />);
+    expect(screen.getByRole('img', { name: 'logo' })).toBeInTheDocument();
+  });
+
+  test('renders navbar links', () => {
+    render(<App />);
+    const homeLinkElement = screen.getByRole('link', { name: /home/i });
+    const superHeroesLinkElement = screen.getByRole('link', {
+      name: /traditional super heroes/i,
+    });
+    const rqSuperHeroesLinkElement = screen.getByRole('link', {
+      name: /rq super heroes/i,
+    });
+
+    expect(homeLinkElement).toBeInTheDocument();
+    expect(superHeroesLinkElement).toBeInTheDocument();
+    expect(rqSuperHeroesLinkElement).toBeInTheDocument();
+
+    expect(homeLinkElement).toHaveAttribute('href', '/');
+    expect(superHeroesLinkElement).toHaveAttribute('href', '/super-heroes');
+    expect(rqSuperHeroesLinkElement).toHaveAttribute(
+      'href',
+      '/rq-super-heroes'
+    );
+  });
 });
